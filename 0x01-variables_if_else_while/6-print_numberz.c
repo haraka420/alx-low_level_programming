bc4daa5c071f279c716661f7932766bc717aaef1
haraka420 <venantsumba1@gmail.com>
2022-09-10 05:52:45 -0700

    commit 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 /**
  * main - print all single numbers
  *
- * Description: program prints " all single numbers
+ * Description: programme prints " all single numbers
  *
  * return: 0
  */
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 for (n = '0'; n <= '9'; n++)
 {
-	putchar ('\n');
+	putchar (n);
 }
 putchar('\n');
 
