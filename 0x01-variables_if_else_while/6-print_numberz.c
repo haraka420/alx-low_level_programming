9fb11aa493634938c1b28a1900ca158c05e17927
haraka420 <venantsumba1@gmail.com>
2022-09-10 05:46:59 -0700

    commitment

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers
+ *
+ * Description: program prints " all single numbers
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int n;
+for (n = '0'; n <= '9'; n++)
+{
+	putchar ('\n');
+}
+putchar('\n');
+
+return (0);
+
+}
