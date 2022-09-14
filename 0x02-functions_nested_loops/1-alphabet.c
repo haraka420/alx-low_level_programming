7fc252702a831c4c93a93c1c9c48fff2526cf670
haraka420 <venantsumba1@gmail.com>
2022-09-14 10:53:20 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,20 @@
  * return: 0
  */
 void print_alphabet(void)
+
 {
+
 	char c;
-for(c = 'a'; c <= 'z'; c++);
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+
+
+
+for (c = 'a'; c <= 'z'; c++)
+
+{
+
+	_putchar(c);
 }
 
+_putchar('\n');
+
+}
