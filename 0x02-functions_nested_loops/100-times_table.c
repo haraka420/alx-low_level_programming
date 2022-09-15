975d10756db6191ee1117db48730eb7f499cc1fa
haraka420 <venantsumba1@gmail.com>
2022-09-15 01:38:25 -0700

    advanced 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,10 @@ void print_times_table(int n)
 
 if (n <= 15 && n >= 0)
 {
-for (digit = 0; digit <= n; digit++)
+for (digit = 0; digit <= n; digit++);
 }
-_putchar('0');
+	_putchar('0');
+
 
 for (mult = 1; mult <= n; mult++)
 
@@ -46,5 +47,4 @@ _putchar((result % 10) + '0');
 }
 _putchar('\n');
 }
-}
-}
+
