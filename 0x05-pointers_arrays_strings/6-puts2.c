c4bc5554dddc5bb3564b2698969cbd31a60d340d
haraka420 <venantsumba1@gmail.com>
2022-09-19 05:29:02 -0700

    elements of arrays of integers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,17 @@
  *
  * Return: nothing
  */
-
 void puts2(char *str)
 {
 	int i = 0;
 
-while (*(str + i) != '\0')
+while (* (str + i) != '\0')
 {
-if (i % 2 == 0)
+	if (i % 2 == 0)
+{
 	putchar(*(str + i));
 	i++;
+}
 }
 putchar(10);
 }
